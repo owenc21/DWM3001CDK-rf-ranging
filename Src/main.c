39ac7cc12bc8e4fa5cbd10e082a20e121c1d840a
owenc21 <owencapell21@gmail.com>
2024-11-09 21:09:49 -0500

    removed unnecessary examples

--- Src/main.c
+++ Src/main.c
@@ -55,22 +55,8 @@ Purpose : DWM3001C build main entry point for simple exmaples.
 #include <stdlib.h>
 #include <assert.h>
 
-/*! ------------------------------------------------------------------------------------------------------------------
- * @fn test_run_info()
- *
- * @brief  This function is simply a printf() call for a string. It is implemented differently on other platforms,
- *         but on the DWM3001C, a printf() call is .
- *
- * @param data - Message data, this data should be NULL string.
- *
- * output parameters
- *
- * no return value
- */
-void test_run_info(unsigned char *data)
-{
-    printf("%s\n", data);
-}
+extern int ss_twr_responder(void);
+extern int ss_twr_initiator(void);
 
 int main(void)
 {
@@ -89,60 +75,9 @@ int main(void)
     /* Small pause before startup */
     nrf_delay_ms(2);
 
-    // UNCOMMENT EXACTLY ONE OF THE BELOW EXAMPLES, AND ALSO UNCOMMENT THE CORRESPONDING #define IN example_selection.h:
-    // extern int read_dev_id(void); read_dev_id();
-    // extern int simple_tx(void); simple_tx();
-    // extern int simple_tx_pdoa(void); simple_tx_pdoa();
-    // extern int simple_rx(void); simple_rx();
-    // extern int simple_rx_nlos(void); simple_rx_nlos();
-    // extern int rx_sniff(void); rx_sniff();
-    // extern int rx_with_xtal_trim(void); rx_with_xtal_trim();
-    // extern int rx_diagnostics(void); rx_diagnostics();
-    // extern int tx_sleep(void); tx_sleep();
-    // extern int tx_sleep_idleRC(void); tx_sleep_idleRC();
-    // extern int tx_timed_sleep(void); tx_timed_sleep();
-    // extern int tx_sleep_auto(void); tx_sleep_auto();
-    // extern int tx_with_cca(void); tx_with_cca();
-    // extern int simple_tx_aes(void); simple_tx_aes();
-    // extern int simple_rx_aes(void); simple_rx_aes();
-    // extern int tx_wait_resp(void); tx_wait_resp();
-    // extern int tx_wait_resp_int(void); tx_wait_resp_int();
-    // extern int rx_send_resp(void); rx_send_resp();
-    extern int ss_twr_responder(void); ss_twr_responder();
-    // extern int ss_twr_initiator(void); ss_twr_initiator();
-    // extern int ss_twr_initiator_sts(void); ss_twr_initiator_sts();
-    // extern int ss_twr_responder_sts(void); ss_twr_responder_sts();
-    // extern int ss_twr_initiator_sts_no_data(void); ss_twr_initiator_sts_no_data();
-    // extern int ss_twr_responder_sts_no_data(void); ss_twr_responder_sts_no_data();
-    // extern int tx_rx_aes_verification(void); tx_rx_aes_verification();
-    // extern int ss_aes_twr_initiator(void); ss_aes_twr_initiator();
-    // extern int ss_aes_twr_responder(void); ss_aes_twr_responder();
-    // extern int ds_twr_initiator(void); ds_twr_initiator();
-    // extern int ds_twr_responder(void); ds_twr_responder();
-    // extern int ds_twr_responder_sts(void); ds_twr_responder_sts();
-    // extern int ds_twr_initiator_sts(void); ds_twr_initiator_sts();
-    // extern int ds_twr_sts_sdc_initiator(void); ds_twr_sts_sdc_initiator();
-    // extern int ds_twr_sts_sdc_responder(void); ds_twr_sts_sdc_responder();
-    // extern int continuous_wave_example(void); continuous_wave_example();
-    // extern int continuous_frame_example(void); continuous_frame_example();
-    // extern int ack_data_rx(void); ack_data_rx();
-    // extern int ack_data_tx(void); ack_data_tx();
-    // extern int gpio_example(void); gpio_example();
-    // extern int simple_tx_sts_sdc(void); simple_tx_sts_sdc();
-    // extern int simple_rx_sts_sdc(void); simple_rx_sts_sdc();
-    // extern int frame_filtering_tx(void); frame_filtering_tx();
-    // extern int frame_filtering_rx(void); frame_filtering_rx();
-    // extern int spi_crc(void); spi_crc();
-    // extern int simple_rx_pdoa(void); simple_rx_pdoa();
-    // extern int otp_write(void); otp_write();
-    // extern int le_pend_tx(void); le_pend_tx();
-    // extern int le_pend_rx(void); le_pend_rx();
-    // extern int pll_cal(void); pll_cal();
-    // extern int bw_cal(void); bw_cal();
-    // extern int double_buffer_rx(void); double_buffer_rx();
-    // extern int timer_example(void); timer_example();
-    // extern int tx_power_adjustment_example(void); tx_power_adjustment_example();
-    // extern int simple_aes(void); simple_aes();
+    // UNCOMMENT EXACTLY ONE OF THE BELOW PROGRAMS
+    // ss_twr_responder();
+    // ss_twr_initiator();
 
     while (1) {}
 }
